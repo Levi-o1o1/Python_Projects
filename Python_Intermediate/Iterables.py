e00b3412d649469032b741ec2355e192cb1acd52
Levi-o1o1 <207032923+Levi-o1o1@users.noreply.github.com>
2026-06-17 12:23:48 +0530

    004

---
+++ Python_Intermediate/Iterables.py
@@ -0,0 +1,19 @@
+# iterables = an collection/object that can return its elements one at a time, allowing
+#             it to be iterated over in a loop 
+
+# numbers = [ 1, 2, 3, 4, 5]
+# for number in numbers:
+#     print(number)
+
+# numbers = [ 1, 2, 3, 4, 5]
+# for number in reversed(numbers):
+#     print(number)
+
+# numbers = ( 1, 2, 3, 4, 5)
+# for number in reversed(numbers):
+#     print(number)
+
+fruts = {"banana", "kwviw","watermelon"}
+
+for furt in fruts:
+    print(furt)
