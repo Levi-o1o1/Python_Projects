8abc75807f650acf36782799904fee3e1b447bd1
Levi-o1o1 <207032923+Levi-o1o1@users.noreply.github.com>
2026-06-18 14:31:03 +0530

    012

---
+++ Python_Intermediate/match_case.py
@@ -0,0 +1,13 @@
+# match case statements
+
+n = input("Enter numbers between(1-7):")
+def day_and_week(day):
+    if n == 1:
+        return "its monday"
+    elif n == 2:
+        return  "its tuesday"
+    elif n == 3:
+        return "its wend"
+
+
+print
