2126c14a4b24a0e8e65de5e581ca9bb7284bef7d
Levi-o1o1 <207032923+Levi-o1o1@users.noreply.github.com>
2026-06-18 13:46:44 +0530

    010

---
+++ Python_Intermediate/list_comp.py
@@ -0,0 +1,13 @@
+# list comprehensions
+
+
+# double = []
+# for x in range(1,11):
+#     double.append(x * 2)
+
+# print(double)
+
+
+# fruits = ["apple", "orange", "banana", "coconut"]
+# fruits = [fruit.upper() for fruit in fruits]
+# print(fruits)
