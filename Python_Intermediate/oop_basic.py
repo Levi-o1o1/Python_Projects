5421d48036d491e98c359618f2dbaf75f82a3ec6
Levi-o1o1 <207032923+Levi-o1o1@users.noreply.github.com>
2026-06-19 09:34:57 +0530

    add use of cons

--- Python_Intermediate/oop_basic.py
+++ Python_Intermediate/oop_basic.py
@@ -11,3 +11,20 @@ class Car():
 m1 = Car()
 print(m1.model)
 print(m1.color)
+
+# classes and constractor 
+
+class Bike:
+    def __init__(self, model,color):
+        self.model = model
+        self.color = color
+        print("Things are doing well :")
+
+
+b1 = Bike("bajaj 150cc", "Green-Black")
+b2 = Bike("activa 100cc", "Grey")
+
+print(b1.model)
+print(b1.color)
+print(b2.model)
+print(b2.color)
