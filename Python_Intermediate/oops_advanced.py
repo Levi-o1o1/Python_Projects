a4c8eb7d70405112c0458f6fd87af04d55ee9a53
Ayush🕸️ <207032923+Levi-o1o1@users.noreply.github.com>
2026-06-20 08:50:42 +0530

    private fuction add

--- Python_Intermediate/oops_advanced.py
+++ Python_Intermediate/oops_advanced.py
@@ -1,13 +1,27 @@
 #  del keyword : used to delete object properties or object itself.
 
-class stu:
-    def __init__(self, name):
-        self.name = name
+# class stu:
+#     def __init__(self, name):
+#         self.name = name
 
 
-s1 = stu("raj")
-print(s1.name)
-del s1.name
-print(s1.name)
+# s1 = stu("raj")
+# print(s1.name)
+# del s1.name
+# print(s1.name)
 
-# private attrivutes & methods
+# private attributes & methods
+#private fuction made for __ after attribute and methods only use for class when we private that 
+
+class person:
+    __name = "unknow"
+
+    def __hello(self):
+        print("Hidden")
+
+    def welcome(self):
+        self.__hello()
+
+p1 = person()
+
+print(p1.welcome())
