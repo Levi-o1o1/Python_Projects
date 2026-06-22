1c2161cd748b95f8343d29da5b4c8968c94d914f
Ayush🕸️ <207032923+Levi-o1o1@users.noreply.github.com>
2026-06-22 21:58:47 +0530

    credit card vali checker

---
+++ Python_Intermediate/credit_card.py
@@ -0,0 +1,14 @@
+# pyhton credit card validator program
+
+# 1 remove any '-' or " "
+# add all digits in the odd places from right to left 
+
+sum_odd = 0
+sum_even = 0
+total = 0 
+
+
+card_num = input("Enter a credit card number :")
+card_num = card_num.replace("-", "")
+card_num = card_num.replace(" ", "")
+print(card_num)
