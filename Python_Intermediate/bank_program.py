43d9483eeae7d334005a8c53ee514f69e1270634
Ayush🕸️ <207032923+Levi-o1o1@users.noreply.github.com>
2026-06-22 23:09:19 +0530

    last commit for bank decoret it

--- Python_Intermediate/bank_program.py
+++ Python_Intermediate/bank_program.py
@@ -1,23 +1,34 @@
 def show_bal(balance):
+    print("**********************************")
     print(f"Your balance is {balance:.2f}")
-
+    print("**********************************")
 def deposit():
+    print("**********************************")
     amount = float(input("Enter an amount to be depostited:"))
+    print("**********************************")
     if amount < 0:
+        print("**********************************")
         print("that's not a valid amount")
+        print("**********************************")
         return 0
     else:
         return amount
      
 
 def withdraw(balance):
+    print("**********************************")
     amount = float(input("Enter amount to be withdrwal:"))
+    print("**********************************")
 
     if amount > balance:
+        print("**********************************")
         print("Insufficient funds")
+        print("**********************************")
         return 0 
     elif amount < 0:
+        print("**********************************")
         print("Amount must be greater than 0 ")
+        print("**********************************")
         return 0
     else:
         return amount
@@ -27,11 +38,14 @@ def main():
     is_run = True
 
     while is_run:
-        print("Banking Program ")
+        print("**********************************")
+        print("        Banking Program           ")
+        print("**********************************")
         print("1. show balacne")
         print("2. Deposit ")
         print("3. Withdraw")
         print("4. Exit ")
+        print("**********************************")
 
         choice = input("Enter your choice (1-4):")
 
@@ -44,9 +58,13 @@ def main():
         elif choice == "4":
             is_run = False
         else:
+            print("**********************************")
             print("That is not a valid choice : please choice again !")
+            print("**********************************")
 
+    print("**********************************")
     print(" Thank you vist again !")    
+    print("**********************************")
 
 if __name__ == '__main__':
     main()
