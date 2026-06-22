f6346b2d8e6c09a5b82f24398b50dafcf7214e6f
Ayush🕸️ <207032923+Levi-o1o1@users.noreply.github.com>
2026-06-22 22:37:51 +0530

    Bank system program 001

--- Python_Intermediate/bank_program.py
+++ Python_Intermediate/bank_program.py
@@ -9,3 +9,11 @@ def withdraw():
 
 balance = 0
 is_run = True
+
+while is_run:
+    print("Banking Program ")
+    print("1. show balacne")
+    print("2. Deposit ")
+    print("3. Withdraw")
+    print("4. Exit ")
+    
